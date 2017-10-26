bf5785df34e3d072a3101ea33e6b76d5d3e38c62
Sebastian Weigand <s.weigand.phy@gmail.com>
2017-10-26 20:38:55 +0200

    finallizing install.sh

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ My zsh config working with putty and windows
 git clone https://github.com/s-weigand/zshrc.git
 
 sudo bash /zshrc/install.sh
+
+open new shell
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 user=${SUDO_USER:-$(whoami)}
-USER_HOME=$(eval echo ~${SUDO_USER})
+USER_HOME=$(eval echo ~${user})
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target=$USER_HOME/testpath
@@ -9,13 +9,6 @@ apt-get update && apt-get dist-upgrade -y
 apt-get install zsh
 chsh -s /bin/zsh
 
-
-
 cp -p $DIR/.zshrc $target/.zshrc
 cp -rp $DIR/.rcs $target/.rcs 
-echo $target
-ls -la $target
-tree -a $target
-rm -rf $target/*
-rm -rf $target/.* 
 
