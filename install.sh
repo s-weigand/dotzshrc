b1466f1a428cca29e9d010dda5e5b71fa102f775
Sebastian Weigand <s.weigand.phy@gmail.com>
2017-10-26 20:46:46 +0200

    fixed test bug in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ user=${SUDO_USER:-$(whoami)}
 USER_HOME=$(eval echo ~${user})
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-target=$USER_HOME/testpath
+target=$USER_HOME
 
 apt-get update && apt-get dist-upgrade -y
 apt-get install zsh
