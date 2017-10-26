499700251aece0ce1d49064591301576301215a1
Sebastian Weigand <s.weigand.phy@gmail.com>
2017-10-26 21:01:56 +0200

    made the shell changing more robust

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ target=$USER_HOME
 
 apt-get update && apt-get dist-upgrade -y
 apt-get install zsh
-chsh -s /bin/zsh $user
+chsh -s $(grep /zsh$ /etc/shells | tail -1) $user
 
 cp -p $DIR/.zshrc $target/.zshrc
 cp -rp $DIR/.rcs $target/.rcs 
