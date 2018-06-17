0cdf1b30ec887b44ca3f364468e010f1fa443eec
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-06-17 23:03:56 +0200

    fixed dir def order in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# defining dirs
 user=${SUDO_USER:-$(whoami)}
 USER_HOME=$(eval echo ~${user})
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # install fonts
 if [ ! -d $USER_HOME/.local/share/fonts ]; then
@@ -9,15 +12,12 @@ fi
 cp -p $DIR/SourceCodePro/*.otf $USER_HOME/.local/share/fonts/SourceCodePro/
 fc-cache -f -v
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-target=$USER_HOME
-
 # install zsh
 apt-get update && apt-get dist-upgrade -y
 apt-get install zsh -y
 chsh -s $(grep /zsh$ /etc/shells | tail -1) $user
 
 # copy config
-cp -p $DIR/.zshrc $target/.zshrc
-cp -rp $DIR/.rcs $target/.rcs 
+cp -p $DIR/.zshrc $USER_HOME/.zshrc
+cp -rp $DIR/.rcs $USER_HOME/.rcs 
 
